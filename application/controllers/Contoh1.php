13b869352621e8ba5ca181ea1375a0127523b919
WildahAlifia <Wildahalif1653@gmail.com>
2022-03-22 01:45:38 +0700

    menambahkan file Latihan1.php, Model_Latihan1.php, Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+	 echo"<h1>Perkenalkan</h1>";
+	 echo"Nama saya Wildah Alifia
+	      Saya tinggal di daerah Bintaro
+	      olah raga yang saya sukai adalah
+	      Berenang";
+	}
+}
